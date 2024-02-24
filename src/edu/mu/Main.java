f207c66a5d222da943861edd2bbb2cf5252e8b80
kriswizzle03 <krislmor21@gmail.com>
2024-02-24 11:08:38 -0600

    Comments outlining code to implement in Main.java

--- src/edu/mu/Main.java
+++ src/edu/mu/Main.java
@@ -3,7 +3,15 @@ package edu.mu;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Instantiate vehicleManager, perform operations based on the requirements.
+		
+		// Read vehicle data from the vehcileList.csv file and initialize objects.
+		
+		// Display all vehicle information.
+		
+		// Display all car information.
+		
+		// Display all motorbike information
 
 	}
 
